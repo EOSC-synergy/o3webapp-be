cb8e12558801acc7ed288f94fdfbcc80d3fef257
valentin.kozlov <valentin.kozlov@kit.edu>
2021-03-26 14:23:55 +0100

    add simplest tests

---
+++ o3webapp_be/test/test_api.py
@@ -0,0 +1,20 @@
+import unittest
+import json
+from flask import request
+
+from o3webapp_be.backend.back_end import app
+
+class TestApi(unittest.TestCase):
+
+    def setUp(self):
+        self.headers = {'Content-Type': 'application/json',
+                        'Accept': 'application/json'}
+
+    def test_model_list_get_returns_302(self):
+        """Test if GET method of the endpoint 
+           /model_list/<ptype> returns 302 (look at another URL)
+        """
+        with app.test_client() as client:
+            response = client.get('/model_list/tco3_zm')
+            # 302 = Tells the client to look at (browse to) another URL
+            assert response._status_code == 302
